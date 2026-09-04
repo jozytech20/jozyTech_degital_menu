af53a656b9fde2788ff72834e7491f4c88c995ac
Samson <samuelgide88@gmail.com>
2026-09-04 22:26:12 +0300

    bbv

--- server/src/controller/auth/auth-controller.js
+++ server/src/controller/auth/auth-controller.js
@@ -43,7 +43,7 @@ export const loginUser = async (req, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000,
   });
 
