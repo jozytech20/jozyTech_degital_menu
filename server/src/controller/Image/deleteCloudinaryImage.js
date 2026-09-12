d7001f4d39eb1f2bd70985c5f1ff621a75e14d6f
Samson <samuelgide88@gmail.com>
2026-09-12 11:44:52 +0300

    feat: add deleteCloudinaryImage controller

--- server/src/controller/Image/deleteCloudinaryImage.js
+++ server/src/controller/Image/deleteCloudinaryImage.js
@@ -1,4 +1,4 @@
-import cloudinary from "../config/cloudinary.js";
+import cloudinary from "../../config/cloudinary.js";
 
 export const deleteCloudinaryImage = async (publicId) => {
     if (!publicId) return;
